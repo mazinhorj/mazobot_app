c8555c0555891adc1e1d1358449ab930f8621746
Osmar Silva <adm.osmarsilva@gmail.com>
2025-06-20 14:26:41 -0300

    out pypdf

--- src/mazobot.py
+++ src/mazobot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from  pypdf import PdfReader
+
 from langchain_groq import ChatGroq
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import WebBaseLoader
